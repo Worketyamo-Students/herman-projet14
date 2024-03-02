13b9f2b9c40b0ea67f106b4801f82dedbddbdc97
monkamherman <151362162+monkamherman@users.noreply.github.com>
2024-03-02 18:20:51 +0100

    1

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          whitethemeselect: '#D1D2DA',
+          whitetheme: '#494C6B',
+          darksthemeselect: '#4D5067',
+          darkstheme: '#CBC8E7',
+          colorsecond: '#9495A5',
+          colorprimary: '#3A7CFD',
+          fond: '#C2C3D6',
+        }
+      }
+    }
+  }
